42c0c0480c708caa09a089cd40c852b897c41033
Minh-Duc Ngo <ngominhduc19@gmail.com>
2023-09-10 11:34:37 +0700

    update Customer.java

--- src/main/java/com/example/demo/entities/Customer.java
+++ src/main/java/com/example/demo/entities/Customer.java
@@ -1,5 +1,10 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.*;
+
 @Entity
+@Table(name = "customer")
 public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
